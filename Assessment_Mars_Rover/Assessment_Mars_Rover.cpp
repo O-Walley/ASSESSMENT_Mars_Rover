0fa7aa2c966a9761d6b6b3b73373a71a69ac45a7
oWalley <2001921@uad.ac.uk>
2025-02-18 17:55:46 +0000

    Setup project to read basic inputs (Grid dimensions, robot start position and operation string)

--- Assessment_Mars_Rover/Assessment_Mars_Rover.cpp
+++ Assessment_Mars_Rover/Assessment_Mars_Rover.cpp
@@ -1,6 +1,39 @@
 #include <iostream>
+#include <string>
+
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    // Get grid dimensions
+    int grid_width, grid_height;
+
+    cout << "Enter grid width: ";
+    cin >> grid_width;
+
+    cout << "Enter grid height: ";
+    cin >> grid_height;
+
+    cout << "Grid dimensions are " << grid_width << "x" << grid_height << "\n" << endl;
+    
+    // Get robot start position and operation string
+    int x_pos, y_pos;
+    char heading;
+    string operations;
+
+    cout << "Enter robot start x: ";
+    cin >> x_pos;
+
+    cout << "Enter robot start y: ";
+    cin >> y_pos;
+
+    cout << "Enter robot start heading: ";
+    cin >> heading;
+
+    cout << "Enter robot operation string: ";
+    cin >> operations;
+
+    cout << "Robot starts at coordinates " << x_pos << "," << y_pos << " heading " << heading << endl;
+    cout << "Robot will perform the following operations: " << operations << endl;
+
 }
